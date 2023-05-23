3db28e9bbaadefbee78fe604a054cac519b4ab62
Sujit <mnamesujit@gmail.com>
2023-05-23 11:37:52 +0530

    updated style and js file

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const btn = document.querySelector("#search-btn");
 const themeMode = document.querySelector("#btn-mode");
 const body = document.querySelector('body');
 
+const userContainer = document.querySelector('#user-container')
+console.log(userContainer)
+console.log("called")
+
 
 const userAvatar = document.querySelector("#user-image");
 const userFullName = document.querySelector("#user-full-name");
@@ -18,21 +22,22 @@ const userLocation = document.querySelector("#location");
 const website = document.querySelector("#website");
 const twitter = document.querySelector("#twitter");
 const company = document.querySelector("#company");
+const email = document.querySelector("#email");
 
 const updateInfo = (data) => {
   userAvatar.src = `${data.avatar_url}`;
   userFullName.innerText = `${data.name}`;
   gitHubUserName.innerText = `@${data.login}`;
   visitProfile.href = `${data.html_url}`;
-  console.log(visitProfile.href)
-  bio.innerText = `${data.bio}`;
+  bio.innerText = data.bio ? `${data.bio}` : `There is no bio of ${data.name}`;
   repoCount.innerText = `${data.public_repos}`;
   followers.innerText = `${data.followers}`;
   following.innerText = `${data.following}`;
-  userLocation.innerText = `${data.location}`;
-  website.innerText = `${data.blog}`;
-  twitter.innerText = `@${data.twitter_username}`;
-  company.innerText = `${data.company}`;
+  userLocation.innerText = data.location ? `${data.location}` : "not-mentioned";
+  website.innerText = data.blog ? `${data.blog}` : "no-blog";
+  twitter.innerText = data.twitter_username ? `@${data.twitter_username}` : "no-username";
+  company.innerText = data.company ? `${data.company}` : "no-company";
+  email.innerText  = data.email ? `${data.email}` : "no-email"
 };
 
 
@@ -48,6 +53,8 @@ const githubData = async (userInput) => {
       // updateInfo(data);
       if(data.login){
         updateInfo(data);
+        userContainer.classList.remove("hidden")
+
       }
       else{
         alert("Username Not Found")
